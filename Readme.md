c3f3ff9ae882dfe5df9610ecacad20239850ecdc
Marco Albero <marco@mybi.es>
2022-03-24 16:24:32 +0100

    First test, collections with threads

---
+++ Readme.md
@@ -0,0 +1,6 @@
+* Test project
+
+** Intention
+
+The purpose of this project is create small programs to test basic concepts
+like concurrency and so.
--- threads/App.java
+++ threads/App.java
@@ -1,23 +1,61 @@
 package es.mybi.test;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+/**
+ Compile: javac -d . App.java
+ Run:     java es/mybi/test/App (ok)
+
+    - with ok param, works as expected due to synchronizedList.
+    - without params, threads overlap values.
+ */
 public class App {
     public static void main(String[] args) {
-	List<Integer> nums = new ArrayList<>();
+
+	List<Integer> nums = Collections.synchronizedList(new ArrayList<>());
+	List<Integer> nums1 = new ArrayList<>();
+
+	String val = "";
+	if(args.length > 0) {
+	    val = args[0];
+	}
+
+	final String v = val;
 
 	Thread t1 = new Thread(new Runnable() {
 		public void run() {
-		    for(int i = 0; i < 1000; i++)
-			nums.add(i);
+		    for(int i = 0; i < 10000; i++)
+			if("ok".equals(v))
+			    nums.add(i);
+		        else
+			    nums1.add(i);
 		}
 	    });
 
 	Thread t2 = new Thread(new Runnable() {
 		public void run() {
-		    for(int i = 0; i < 1000; i++)
-			nums.add(i);
+		    for(int i = 0; i < 10000; i++)
+			if("ok".equals(v))
+			    nums.add(i);
+		        else
+			    nums1.add(i);
 		}
 	    });
 
 	t1.start();
 	t2.start();
+
+	try {
+	    t1.join();
+	    t2.join();
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	}
+
+	System.out.println(String.format("Size of array: %s", nums.size()));
+	System.out.println(String.format("Size of array: %s", nums1.size()));
+    }
 }
